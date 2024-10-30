28a0d4fa925dcecc3227a0564d01ef4baaf9a835
Aaron Perez <aarperper@gmail.com>
2024-10-30 17:59:59 +0000

    red: can record that the position 1 is marked by player 1 and position 2 by player 2

--- src/TicTacToe.spec.ts
+++ src/TicTacToe.spec.ts
@@ -35,4 +35,12 @@ describe(TicTacToe, () => {
 
     expect(ticTacToe.getBoard()).toEqual("1|X|3\n-----\n4|5|6\n-----\n7|8|9")
   })
+  it("can record that the position 1 is marked by player 1 and position 2 by player 2", () => {
+    const ticTacToe = new TicTacToe()
+
+    ticTacToe.playIn(1)
+    ticTacToe.playIn(2)
+
+    expect(ticTacToe.getBoard()).toEqual("X|O|3\n-----\n4|5|6\n-----\n7|8|9")
+  })
 })
