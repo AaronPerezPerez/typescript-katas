434ccc7f2bf460ef59ca105cdcfd5104f4f6a4dd
Aaron Perez <aarperper@gmail.com>
2024-10-30 18:44:48 +0000

    green: ends when player 2 has three symbols in a row

--- src/TicTacToe.ts
+++ src/TicTacToe.ts
@@ -9,6 +9,8 @@ export class TicTacToe {
 
   getStatus() {
     if (this.doesPlayer1Win()) return "p1 wins"
+    if (this.doesPlayer2Win()) return "p2 wins"
+
     return "playing"
   }
 
@@ -16,6 +18,12 @@ export class TicTacToe {
     const columnRegex =
       /^(X\|.\|.\n-----\nX\|.\|.\n-----\nX\|.\|.$)|(.\|X\|.\n-----\n.\|X\|.\n-----\n.\|X\|.$)|(.\|.\|X\n-----\n.\|.\|X\n-----\n.\|.\|X$)/
     return this.board.includes("X|X|X") || this.board.match(columnRegex)
+  }
+
+  private doesPlayer2Win() {
+    const columnRegex =
+      /^(O\|.\|.\n-----\nO\|.\|.\n-----\nO\|.\|.$)|(.\|O\|.\n-----\n.\|O\|.\n-----\n.\|O\|.$)|(.\|.\|O\n-----\n.\|.\|O\n-----\n.\|.\|O$)/
+    return this.board.includes("O|O|O") || this.board.match(columnRegex)
   }
 
   playIn(position: number) {
