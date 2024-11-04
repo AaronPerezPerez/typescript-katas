d02212513526b7151dc3acdf735cebb34bb06f08
Aaron Perez <aarperper@gmail.com>
2024-11-04 14:24:37 +0000

    refactor: cleans the code

--- src/TicTacToe.ts
+++ src/TicTacToe.ts
@@ -1,43 +1,50 @@
+type GameStatus = "p1 wins" | "p2 wins" | "draw" | "playing"
+
 export class TicTacToe {
   private board = "1|2|3\n-----\n4|5|6\n-----\n7|8|9"
-
+  private player1 = "X"
+  private player2 = "O"
   private isPlayer1Turn = true
+
+  playIn(position: number) {
+    if (!this.board.includes(position.toString())) return
+
+    if (this.isPlayer1Turn) this.board = this.board.replace(position.toString(), this.player1)
+    if (!this.isPlayer1Turn) this.board = this.board.replace(position.toString(), this.player2)
+    this.isPlayer1Turn = !this.isPlayer1Turn
+  }
 
   getBoard() {
     return this.board
   }
 
-  getStatus() {
-    if (this.doesPlayer1Win()) return "p1 wins"
-    if (this.doesPlayer2Win()) return "p2 wins"
+  getStatus(): GameStatus {
+    const currentPlayer = this.getCurrentPlayer()
+    if (this.doesPlayer1Win(this.player1)) return "p1 wins"
+    if (this.doesPlayer1Win(this.player2)) return "p2 wins"
     if (this.doesTheGameIsDraw()) return "draw"
 
     return "playing"
   }
 
+  private getCurrentPlayer() {
+    return this.isPlayer1Turn ? this.player1 : this.player2
+  }
   private doesTheGameIsDraw() {
     return this.board.match(/[0-9]/) == null
   }
 
-  private doesPlayer1Win() {
-    const columnRegex =
-      /^(X\|.\|.\n-----\nX\|.\|.\n-----\nX\|.\|.$)|(.\|X\|.\n-----\n.\|X\|.\n-----\n.\|X\|.$)|(.\|.\|X\n-----\n.\|.\|X\n-----\n.\|.\|X$)/
-    const diagonalRegex = /^(X\|.\|.\n-----\n.\|X\|.\n-----\n.\|.\|X$)|(.\|.\|X\n-----\n.\|X\|.\n-----\nX\|.\|.$)/
-
-    return this.board.includes("X|X|X") || this.board.match(columnRegex) || this.board.match(diagonalRegex)
-  }
-
-  private doesPlayer2Win() {
-    const columnRegex =
-      /^(O\|.\|.\n-----\nO\|.\|.\n-----\nO\|.\|.$)|(.\|O\|.\n-----\n.\|O\|.\n-----\n.\|O\|.$)|(.\|.\|O\n-----\n.\|.\|O\n-----\n.\|.\|O$)/
-    return this.board.includes("O|O|O") || this.board.match(columnRegex)
-  }
-
-  playIn(position: number) {
-    if (!this.board.includes(position.toString())) return
-
-    if (this.isPlayer1Turn) this.board = this.board.replace(position.toString(), "X")
-    if (!this.isPlayer1Turn) this.board = this.board.replace(position.toString(), "O")
-    this.isPlayer1Turn = !this.isPlayer1Turn
+  private doesPlayer1Win(player: string) {
+    const columnRegex = new RegExp(
+      `^(${player}\\|.\\|.\\n-----\\n${player}\\|.\\|.\\n-----\\n${player}\\|.\\|.$)|(.\\|${player}\\|.\\n-----\\n.\\|${player}\\|.\\n-----\\n.\\|${player}\\|.$)|(.\\|.\\|${player}\\n-----\\n.\\|.\\|${player}\\n-----\\n.\\|.\\|${player}$)`,
+    )
+    const diagonalRegex = new RegExp(
+      `^(${player}\\|.\\|.\\n-----\\n.\\|${player}\\|.\\n-----\\n.\\|.\\|${player}$)|(.\\|.\\|${player}\\n-----\\n.\\|${player}\\|.\\n-----\\n${player}\\|.\\|.$)`,
+    )
+    return (
+      this.board.includes(`${player}|${player}|${player}`) ||
+      this.board.match(columnRegex) ||
+      this.board.match(diagonalRegex)
+    )
   }
 }
